bdcf24824ff64e278ffe1220a59fd3379747f8a4
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-11-10 17:01:03 +0100

    set condition for the title and add little modify to style of the page

--- src/App.jsx
+++ src/App.jsx
@@ -37,41 +37,55 @@ export default function App() {
       titolo: newArticleTitle
     }
 
-    setArticlesList(articlesList => [...articlesList, newArticle])
+    if (newArticleTitle.length > 0) {
+      setArticlesList(articlesList => [...articlesList, newArticle])
+      setNewArticleTitle("")
+    } else {
+      alert("Scrivi qualcosa nel form")
+    }
+  }
 
+  function handleDelete(idToDelete) {
+    const filteredArticle = articlesList.filter(article => idToDelete != article.id)
 
-
+    setArticlesList(filteredArticle)
   }
 
   return (
     <>
       <header>
-        <h1 className='text-center fw-bold my-3'>Future article</h1>
+        <h1 className='text-center fw-bold py-4'>MY JOURNAL</h1>
       </header>
 
       <main>
         <div className="container">
           <form onSubmit={handleSubmit} className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Add article title.." aria-label="article-title" aria-describedby="button-add-article" value={newArticleTitle} onChange={e => setNewArticleTitle(e.target.value)} />
+            <input type="text" className="form-control box-shadow-none" placeholder="Add article title.." aria-label="article-title" aria-describedby="button-add-article" value={newArticleTitle} onChange={e => setNewArticleTitle(e.target.value)} />
             <button className="btn btn-success" type="submit" id="btn-add-article">Add</button>
           </form>
-          <div className="card">
-            <div className="card-header text-center fw-bold">
-              <h3>MY ARTICLE</h3>
-            </div>
-            <ul className='list-group list-group-flush '>
-              {
-                articlesList.map(article => (
+          {articlesList.length > 0 &&
+            <div className="card">
+              <div className="card-header text-center fw-bold">
+                <h3>My article</h3>
+              </div>
+              <ul className='list-group list-group-flush '>
+
+                {articlesList.map(article => (
                   <li className='list-group-item list-unstyled d-flex justify-content-between align-items-center' key={article.id}>
                     <p className='mb-0 py-3 '>{article.titolo}</p>
-                    <button className='btn btn-danger'>
+                    <button className='btn btn-danger' onClick={() => handleDelete(article.id)}>
                       <i className="bi bi-trash"></i>
                     </button>
                   </li>
-                ))
-              }
-            </ul>
-          </div>
+                ))}
+
+              </ul>
+            </div>
+          }
+          {
+            articlesList.length == 0 &&
+            <h3 className='text-center text-dark mt-5'>Al momento non ci sono articoli nella lista!</h3>
+          }
         </div>
       </main>
     </>
